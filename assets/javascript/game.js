9fcd0d2efa8e88be2ed378680b7103b9b5b7ed72
SharonC <sharon.s.c.chien@gmail.com>
2019-01-30 23:21:06 -0500

    browser crash

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -32,16 +32,11 @@
 
 // picke a random word from the list
     var randomSpell = spell[Math.floor(Math.random()*spell.length)]; // this randomly selects form the array
-    console.log(randomSpell);
 // help // store that random word in a variable in the form of a question
     var question = [];
     for (var i = 0; 1 < randomSpell.length; i++){
-        question[i] = "_";
+        question.push("_");
     }
-    var remainingLetters = spell.length;
-    
-    console.log(question);
-
 
 // define a variable that stores total # of tries
     var tries = 10;
@@ -52,34 +47,16 @@
     // use if else statment to also assign "1" as end game key
 
 // listen to the key event
-    //window.addEventListener("keyup", this) {
-    //    keyof
-    //    return letter
-    //}
 // store key event
-    //var letter = []; //user entered key/letter
-    //push ???
 // check if key pressed matches at each position of the word
 // revewal positions matching key pressed
 // else     decrease total number of tries by one
-    //for (var j = 0; j < question.length; j++){
-        //if (letter === charAt(j)){
-            //function to display letter
-            //else 
-            // function tries - 1
-    //    }
-    //}
 
 // if total number of tries = 0
 // then score = score -1
 // reset guesses to be total number of guesses
 // choose another random word
 // repeat game
-//if (parseInt(tries) === 0){
-    //var loss = loss + 1;
-    //var tries = 10;
-    //functiont that starts game
-//}
 
 // if word is guessed by user
 // then score = sore + 1
