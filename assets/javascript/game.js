1134f603c8cfbc1eeeb8cc6fd9e81852f66b5586
SharonC <sharon.s.c.chien@gmail.com>
2019-02-02 00:08:12 -0500

    still trying

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,5 @@
 // problem? guess the word
+"use strict";
 
 // create a list of definded words that can be guesssed
     var spell = [
@@ -30,54 +31,110 @@
     "evanesco",
 ];
 
-// picke a random word from the list
-    var randomSpell = spell[Math.floor(Math.random()*spell.length)]; // this randomly selects form the array
-// store that random word in a variable in the form of a question
-    var question = [];
-    for (var i = 0; i < randomSpell.length; i++){
-        question.push("_");
+const maxTries = 10; // max # of tries
+var letter = "";
+
+// create entire game object
+var game = {
+    spellList: spell, // property holding list
+    guessedLetters: [], // holds unique letters guessed
+    randomSpell: "", // the selected spell
+    correctGuess: [], // chars guessed correctly
+    win: 0, // total wins
+    loss: 0, // total losses
+    remainingTries: 0, // remaining guesses set to 0
+    hasFinished: true, // if won or loss
+    winSound: new Audio(), // holds win sound
+    loseSound: new Audio(), // holds lose sound
+
+    // reset game function that resets all variables
+    resetGame: function (){
+        this.remainingTries = maxTries;
+        this.hasFinished = false;
+        this.guessedLetters = [];
+        this.correctGuess = [];
+
+        this.randomSpell = this.spellList[Math.floor(Math.random()*this.spellList.length)]; // this randomly selects a spell
+
+        // build the spell for guessing
+        for (var i = 0; i < this.randomSpell.length; i++){
+            this.correctGuess.push(" _ ");
+        }
+        this.updateDisplay();
+    },
+
+    // updae Display function. updates the HTML display areas (win, loss, guesses, correct guesses)
+    updateDisplay: function(){
+        var progress = "";
+        for (var i = 0; i < this.randomSpell.length; i++){
+            progress += this.correctGuess[i];
+        }
+        document.querySelector("#showQuestion").innerHTML = progress;
+        document.querySelector("#showTries").innerHTML = this.remainingTries;
+        document.querySelector("#showGuess").innerHTML = this.guessedLetters;
+        document.querySelector("#showWin").innerText = this.win;
+        document.querySelector("#showLoss").innerText = this.loss;
+    },
+
+    // track user's guess
+    userGuess: function (letter) {
+        if (this.remainingTries > 0){
+            if (this.guessedLetter.indexOf(letter) === -1) {
+                this.guessedLetter.push(letter);
+                this.matchGuess(letter);
+            }
+        }
+        this.checkSpell();
+        this.updateDisplay();
+    },
+
+    //Scan user's guess through function matchGuess
+    matchGuess: function(letter){
+        var position = [];
+        for (var i = 0; i < this.randomSpell.length; i++) {
+            if (this.randomSpell[i] === letter){
+                position.push(i);
+            }
+            
+        }
+
+        if (position.length <=0){
+            this.remaingingGuesses--;            
+        } else {
+            for (var i = 0; i < position.length; i++){
+                this.correctGuess[position[i]] = letter;
+            }
+        }
+    },
+
+    // check if user guess all letters in word
+    checkSpell: function (){
+        //if win
+        if (this.randomSpell.indexOf(" _ ") === -1){
+            this.win++
+            this.winSound.play();
+            this.hasFinished = true;
+            return;
+        }
+        //if lose
+        if (this.remainingTries == 0){
+            this.loseSound.play();
+            this.loss++
+            this.hasFinished = true;
+            return
+        }
+    },
+};
+
+// Listen to user key input
+window.addEventListener("keyup", function(event){
+    if (game.hasFinished) {
+        game.resetGame();
+        game.hasFinished = false;
+    } else {
+        if (event.keyCode >= 65 && event.keyCode <=90){
+            letter = event.key.toLowerCase();
+            game.userGuess(letter);
+        }
     }
-
-    console.log(randomSpell);
-    console.log(question);
-
-// define a variable that stores total # of tries
-    var tries = 10;
-    document.getElementById("showTries").innerText = tries;
-
-
-// have user enter a key
-    // Include "Press any key to get started" within HTML.
-    // see if i can get program to flash this line until user enters a key
-    // use if else statment to also assign "1" as end game key
-
-
-    
-// listen to the key event for guesses
-    var guesses = [];
-    window.addEventListener("keyup",function(e){
-        // stores only alphabets and no recurring characters
-        if (event.keyCode <= 90 && event.keyCode >= 65 && guesses.indexOf(event.key) == -1){
-            guesses.push(event.key.toLowerCase());
-            document.getElementById("showGuess").innerHTML = guesses;
-        };
-    })
-        // check if key pressed matches at each position of the word
-
-// display guesses
-
-// reveal positions matching key pressed
-// else     decrease total number of tries by one
-
-
-// if total number of tries = 0
-// then score = score -1
-// reset guesses to be total number of guesses
-// choose another random word
-// repeat game
-
-// if word is guessed by user
-// then score = sore + 1
-// reset gueses to be total number of guesses
-// choose another random word
-// repeat game
+})
